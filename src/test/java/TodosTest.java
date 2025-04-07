4eddd5275f6877dd8cb9126f3d806a6e5c38dcad
Tohage89 <avgorin89@gmail.com>
2025-04-07 17:09:05 +0300

    add new test shouldFindThreeTasks

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -120,4 +120,29 @@ public class TodosTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void shouldFindThreeTasks() {
+        SimpleTask simpleTask = new SimpleTask(5, "Собрать проект");
+
+        String[] subtasks = {"Молоко", "проект", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатить проект",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.search("проект");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
