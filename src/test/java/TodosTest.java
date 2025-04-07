6e30baf6f0c405d9ddece87b2d067087b79ae6c2
Tohage89 <avgorin89@gmail.com>
2025-04-07 13:42:17 +0300

    revision3

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -29,12 +29,7 @@ public class TodosTest {
     }
 
     @Test
-    public void shouldFindOneTaskByQuery() {
-        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
-
-        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
-        Epic epic = new Epic(55, subtasks);
-
+    public void shouldFindSEpicByTopic() {
         Meeting meeting = new Meeting(
                 555,
                 "Выкатка 3й версии приложения",
@@ -44,9 +39,56 @@ public class TodosTest {
 
         Todos todos = new Todos();
 
-        todos.add(simpleTask);
-        todos.add(epic);
         todos.add(meeting);
+
+
+        Task[] expected = {meeting};
+        Task[] actual = todos.search("Выкатка");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindSEpicByProject() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(meeting);
+
+
+        Task[] expected = {meeting};
+        Task[] actual = todos.search("НетоБанка");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldFindSMeetingByQuery() {
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Todos todos = new Todos();
+
+        todos.add(epic);
+
+
+        Task[] expected = {epic};
+        Task[] actual = todos.search("Яйца");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldFindSimpleTaskByQuery() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+
 
         Task[] expected = {simpleTask};
         Task[] actual = todos.search("Позвонить");
